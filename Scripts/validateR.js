ecdc1b280e976c04d7e0eefe6ada05c9de5f2e5d
GuascaC <109239941+GuascaC@users.noreply.github.com>
2022-11-22 16:57:48 -0500

    Add files via upload

--- Scripts/validateR.js
+++ Scripts/validateR.js
@@ -1,7 +1,7 @@
 'use strict';
 const email = document.querySelector("#iMail");
 const CC = document.querySelector("#iCC");
-const rol = document.querySelector("#iRol");
+const Rol = document.querySelector("#iRol");
 const pass = document.querySelector("#iPass");
 const passC = document.querySelector("#iPassC");
 const form = document.querySelector("#form");
@@ -14,7 +14,7 @@ form,addEventListener("submit", (e)=>{
         modalAlerta("Error");
 }
 
-})
+});
 
 const validFieldsForm = () =>{
     const values = Object.values(validFields);
@@ -35,7 +35,7 @@ let validFields = {
 
 email.addEventListener('change', function(e) {
     const inputEmail = e.target.value;
-    const patronEmail = /^[a-zA-Z.]+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm; //Santiago
+    const patronEmail = /^[a-zA-Z.]+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm;
     validFields.email = inputEmail.match(patronEmail) ? true: false;
     console.log(Object.values(validFields));
 
@@ -43,31 +43,28 @@ email.addEventListener('change', function(e) {
 CC.addEventListener('change', function(e) {
     const inputCC = e.target.value;
     const patronCC = /^[0-9]{7,10}$/gm; 
-    validFields.email = inputCC.match(patronCC) ? true: false;
+    validFields.CC = inputCC.match(patronCC) ? true: false;
     console.log(Object.values(validFields));
 });
-rol.addEventListener('change', function(e) {
-    if(rol.value === "ok"){  
-        validFields.rol = rol.value ? true : false;
-        console.log(Object.values(validFields));}
-        else{  
-        validFields.rol = ciudad.value ? false : true;
-        console.log(Object.values(validFields));}
+Rol.addEventListener('change', function(e) {
+    validFields.rol = Rol.selectedIndex > 0 ? true : false;
+    console.log(Object.values(validFields));
 });
 pass.addEventListener('change',(event)=>{
     const inputPass = event.target.value; 
-    const patronPass = /([a-zA-Z0-9]{8,15})$/gm; 
+    const patronPass = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,15})/gm; 
     validFields.pass = inputPass.match(patronPass) ? true: false;
     console.log(Object.values(validFields));
 });
 passC.addEventListener('change',(event)=>{
     const inputPassC = event.target.value; 
-    const patronPassC = /([a-zA-Z0-9]{8,15})$/gm; 
+    const patronPassC = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,15})/gm; 
     validFields.pass = inputPassC.match(patronPassC) ? true: false;
-    if (validFields.pass === false){
+    if (passC.value !== pass.value){
         modalAlerta("Las contraseñas no coinciden");
-    } 
-    console.log(Object.values(validFields));
+    }else{ 
+      console.log(Object.values(validFields));  
+    }
 });
 
 function modalAlerta(cadena){
