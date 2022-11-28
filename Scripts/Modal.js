0aa504ea1f6b15ee6b74b29e6f49c914cd89e78a
GuascaC <109239941+GuascaC@users.noreply.github.com>
2022-11-28 12:48:38 -0500

    Update Modal.js

--- Scripts/Modal.js
+++ Scripts/Modal.js
@@ -1,14 +1,2 @@
 'use strict';
-if (document.getElementById(Modal)){
-
-    let modal = document.getElementById
-    let button = document.getElementById
-    let span = document.getElementsByClassName
-    let body = document.getElementsByTagName
-
-    button.onclick = function(){
-        modal.style
-    }
-
-}
  
