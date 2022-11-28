8faa6695dd6e1a7947aaecb69b40efced9be752f
GuascaC <109239941+GuascaC@users.noreply.github.com>
2022-11-28 14:04:42 -0500

    Update Redirigir.js

--- Scripts/Redirigir.js
+++ Scripts/Redirigir.js
@@ -20,4 +20,12 @@ function redirigir06(){
 function redirigir07(){
     window.location.href="../Certificado-main/CContraseña.html";
  }
- 
+const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+const btnAbrirModal = document.querySelector("#btn-abrir-modal");
+const modal = document.querySelector("#modal");
+
+btnAbrirModal.addEventListener("click",(e)=>{
+    e.preventDefault();
+    modal.showModal();})
+btnCerrarModal.addEventListener("click",()=>{
+    modal.close();});
