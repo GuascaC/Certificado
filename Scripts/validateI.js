7ed07a9ca2159653cb30ca03352fe628cc46d7a3
GuascaC <109239941+GuascaC@users.noreply.github.com>
2022-12-15 14:17:09 -0600

    Add files via upload

--- Scripts/validateI.js
+++ Scripts/validateI.js
@@ -4,76 +4,92 @@ const pass = document.querySelector("#ipass");
 const form = document.querySelector("#form");
 const rol = document.querySelector("#iRol");
 
-form. addEventListener("submit", (e) => {
+form, addEventListener("submit", (e) => {
   e.preventDefault();
   if (validFieldsForm() === -1) {
-    if(rol.value == 1){
-            window.location.href="../../Certificado-main/Certificado-main/perfiladministrador.html";
-        }
-        else if(rol.value == 2){
-            window.location.href="../../Certificado-main/Certificado-main/Docente.html";
-        }
-        else if(rol.value == 3){
-            window.location.href="../../Certificado-main/Certificado-main/Catedratico.html";
-        }
-        else if(rol.value == 4){
-            window.location.href="../../Certificado-main/Certificado-main/Practicante.html";
-        }else{
-        modalAlerta("Error");
-        }
+    if (rol.value == 1) {
+      console.log("1");
+      window.location.href = "../../Certificado-main/perfiladministrador.html";
+    }
+    else if (rol.value == 2) {
+      console.log("2");
+      window.location.href = "../../Certificado-main/Docente.html";
+    }
+    else if (rol.value == 3) {
+      console.log("3");
+      window.location.href = "../../Certificado-main/Catedratico.html";
+    }
+    else if (rol.value == 4) {
+      console.log("4");
+      window.location.href = "../../Certificado-main/Practicante.html";
+    }
+  } else {
+    modalAlerta("Error");
+  }
+
+})
+
+const validFieldsForm = () => {
+  const values = Object.values(validFields);
+  let response = values.findIndex(e => e === false);
+  return response;
+}
+
+//Objeto de Validación
+
 let validFields = {
-    email:false,
-    pass: false
-    pass: false,
-    rol: false
+  email: false,
+  pass: false
 
 }
-  email.addEventListener('change', function(e) {
-    validFields.email = inputEmail.match(patronEmail) ? true: false;
-    console.log(Object.values(validFields));
-    if (validFields.email===false){
-      modalAlerta("El correo no es institucional");
-        modalAlerta("El correo no es institucional");
-   }
+
+email.addEventListener('change', function(e) {
+  const inputEmail = e.target.value;
+  const patronEmail = /^[a-zA-Z.]+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm
+  validFields.email = inputEmail.match(patronEmail) ? true : false;
+  console.log(Object.values(validFields));
+  if (validFields.email === false) {
+    modalAlerta("El correo no es institucional");
+  }
 
 });
-pass.addEventListener('change',(e)=>{
-    const inputPass = e.target.value; 
-    const patronPass = /([a-zA-Z0-9]{8,})$/g; 
-    validFields.pass = inputPass.match(patronPass) ? true: false;
-    console.log(Object.values(validFields));
+pass.addEventListener('change', (e) => {
+  const inputPass = e.target.value;
+  const patronPass = /([a-zA-Z0-9]{8,})$/g;
+  validFields.pass = inputPass.match(patronPass) ? true : false;
+  console.log(Object.values(validFields));
 });
 
-rol.addEventListener('change', (e)=>{
-    validFields.rol = rol.selectedIndex > 0 ? true : false;
-    console.log(Object.values(validFields));
-});
-
-function modalAlerta(cadena){
-    const alert=document.createElement("div");
-    const texto=document.createElement("p");
-    texto.setAttribute("Class","textAlerta");
-    alert.setAttribute("id","alerta");
-    alert.setAttribute("class","alerta");
-    texto.innerHTML=`<strong>${cadena}</strong>`;
-    alert.appendChild(texto);
-    document.body.appendChild(alert);
-    alert.onclick=function(){
-        document.getElementById("alerta").remove();
-    }
+function modalAlerta(cadena) {
+  const alert = document.createElement("div");
+  const texto = document.createElement("p");
+  const btn = document.createElement("button");
+  btn.setAttribute("Class", "btnAlerta");
+  texto.setAttribute("Class", "textAlerta");
+  alert.setAttribute("id", "alerta");
+  alert.setAttribute("class", "alerta");
+  texto.innerHTML = `<strong>${cadena}</strong>`;
+  btn.innerHTML = `cerrar`;
+  alert.appendChild(texto);
+  alert.appendChild(btn);
+  document.body.appendChild(alert);
+  btn.onclick = function() {
+    document.getElementById("alerta").remove();
+  }
 }
 function PassV02() {
-    var x = document.getElementById("iPass");
-    if (x.type === "password") {
-        x.type = "text";
-        } else {
-        x.type = "password";
-    }
-  }
-function PassV03() {
+  var x = document.getElementById("iPass");
   if (x.type === "password") {
-        x.type = "text";
-        } else {
-        x.type = "password";
-        }
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+function PassV03() {
+  var x = document.getElementById("iPass");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
--- Scripts/validateR.js
+++ Scripts/validateR.js
@@ -5,116 +5,120 @@ const Rol = document.querySelector("#iRol");
 const pass = document.querySelector("#ipass");
 const passC = document.querySelector("#ipassC");
 const form = document.querySelector("#form");
-const conf=document.getElementById("cntn");
+const conf = document.getElementById("cntn");
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    if (validFieldsForm() === -1){ 
-        modalAlerta("Información Validada");
-        window.location.href="../index.html";
-    }else {
-        modalAlerta("Error");
-}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (validFieldsForm() === -1) {
+    modalAlerta("Información Validada");
+    window.location.href = "../index.html";
+  } else {
+    modalAlerta("Error");
+  }
 
 });
 
-const validFieldsForm = () =>{
-    const values = Object.values(validFields);
-    let response = values.findIndex(e=> e === false);
-    return response;
+const validFieldsForm = () => {
+  const values = Object.values(validFields);
+  let response = values.findIndex(e => e === false);
+  return response;
 }
 
 //Objeto de Validación
 
 let validFields = {
-    email:false,
-    CC:false,
-    rol:false,
-    pass: false,
-    passC: false
+  email: false,
+  CC: false,
+  rol: false,
+  pass: false,
+  passC: false
 
 }
 
 email.addEventListener('change', function(e) {
-    const inputEmail = e.target.value;
-    const patronEmail = /^[a-zA-Z.]+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm;
-    validFields.email = inputEmail.match(patronEmail) ? true: false;
-    console.log(Object.values(validFields));
-    if(validFields.email===false){
-      modalAlerta("El correo no es institucional");
-    }
-    console.log(Object.values(validFields));
+  const inputEmail = e.target.value;
+  const patronEmail = /^[a-zA-Z.]+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm;
+  validFields.email = inputEmail.match(patronEmail) ? true : false;
+  console.log(Object.values(validFields));
+  if (validFields.email === false) {
+    modalAlerta("El correo no es institucional");
+  }
+  console.log(Object.values(validFields));
 
 });
 CC.addEventListener('change', function(e) {
-    const inputCC = e.target.value;
-    const patronCC = /^[0-9]{10}$/gm;
-    if (CC.value.length > 10) {
-        CC.value = CC.value.slice(0,10);}//Condicional
-    validFields.CC = inputCC.match(patronCC) ? true: false;
-    console.log(Object.values(validFields));
+  const inputCC = e.target.value;
+  const patronCC = /^[0-9]{10}$/gm;
+  if (CC.value.length > 10) {
+    CC.value = CC.value.slice(0, 10);
+  }//Condicional
+  validFields.CC = inputCC.match(patronCC) ? true : false;
+  console.log(Object.values(validFields));
 });
 Rol.addEventListener('change', function(e) {
-    validFields.rol = Rol.selectedIndex > 0 ? true : false;
-    if(validFields.rol==false){
-      modalAlerta("Seleccione un rol")
-    }
+  validFields.rol = Rol.selectedIndex > 0 ? true : false;
+  if (validFields.rol == false) {
+    modalAlerta("Seleccione un rol")
+  }
   console.log(Object.values(validFields));
-    console.log(Object.values(validFields));
 });
 
-pass.addEventListener('change',(e)=>{
-    const inputPass = e.target.value; 
-    const patronPass = /([a-zA-Z0-9]{8,})$/g; 
-    validFields.pass = inputPass.match(patronPass) ? true: false;
-    console.log(Object.values(validFields));
+pass.addEventListener('change', (e) => {
+  const inputPass = e.target.value;
+  const patronPass = /([a-zA-Z0-9]{8,})$/g;
+  validFields.pass = inputPass.match(patronPass) ? true : false;
+  console.log(Object.values(validFields));
 });
 
-passC.addEventListener('change',(event)=>{
-    if (pass.value === passC.value) {
-        validFields.passC =true;
-        passC.value = passC.value.slice(0,15);
-      }else{
-      modalAlerta("Las contraseñas no coinciden")
-    }
-    console.log(Object.values(validFields));  
+passC.addEventListener('change', (event) => {
+  if (pass.value === passC.value) {
+    validFields.passC = true;
+    passC.value = passC.value.slice(0, 15);
+  } else {
+    modalAlerta("Las contraseñas no coinciden")
+  }
+  console.log(Object.values(validFields));
 });
 
-function modalAlerta(cadena){
-    const alert=document.createElement("div");
-    const texto=document.createElement("p");
-    texto.setAttribute("Class","textAlerta");
-    alert.setAttribute("id","alerta");
-    alert.setAttribute("class","alerta");
-    texto.innerHTML=`<strong>${cadena}</strong>`;
-    alert.appendChild(texto);
-    document.body.appendChild(alert);
-    alert.onclick=function(){
-        document.getElementById("alerta").remove();
-    }
+function modalAlerta(cadena) {
+  const alert = document.createElement("div");
+  const texto = document.createElement("p");
+  const btn = document.createElement("button");
+  btn.setAttribute("Class", "btnAlerta");
+  texto.setAttribute("Class", "textAlerta");
+  alert.setAttribute("id", "alerta");
+  alert.setAttribute("class", "alerta");
+  texto.innerHTML = `<strong>${cadena}</strong>`;
+  btn.innerHTML = `cerrar`;
+  alert.appendChild(texto);
+  alert.appendChild(btn);
+  document.body.appendChild(alert);
+  btn.onclick = function() {
+    document.getElementById("alerta").remove();
+  }
 }
 function PassV() {
-   var Passv = document.getElementById("ipass");
-    if (Passv.type === "password") {
-        console.log("Funciona")
-      Passv.type = "text";
-    } else {
-      Passv.type = "password";
-    }
+  var Passv = document.getElementById("ipass");
+  if (Passv.type === "password") {
+    console.log("Funciona")
+    Passv.type = "text";
+  } else {
+    Passv.type = "password";
+  }
 }
 function PassV02() {
-    let Passv = document.getElementById("ipassC");
-     if (Passv.type === "password") {
-       Passv.type = "text";
-     } else {
-       Passv.type = "password";
-     }
+  let Passv = document.getElementById("ipassC");
+  if (Passv.type === "password") {
+    Passv.type = "text";
+  } else {
+    Passv.type = "password";
+  }
 }
 function PassV03() {
-    let Passv = document.getElementById("ipassN");
-     if (Passv.type === "password") {
-       Passv.type = "text";
-     } else {
-       Passv.type = "password";
-     }
+  let Passv = document.getElementById("ipassN");
+  if (Passv.type === "password") {
+    Passv.type = "text";
+  } else {
+    Passv.type = "password";
+  }
 }
