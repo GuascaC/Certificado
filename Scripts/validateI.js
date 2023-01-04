ceac6bbf099e946d7f136e90d3907181f21d2434
Santsjj <109623621+Santsjj@users.noreply.github.com>
2023-01-04 10:41:44 -0600

    Add files via upload

--- Scripts/validateI.js
+++ Scripts/validateI.js
@@ -1,4 +1,6 @@
+//Validación para iniciar sesión
 'use strict';
+//Se trae los inputs para validar la información 
 const email = document.querySelector("#iMail");
 const pass = document.querySelector("#ipass");
 const form = document.querySelector("#form");
@@ -7,36 +9,40 @@ const rol = document.querySelector("#iRol");
 form, addEventListener("submit", (e) => {
   e.preventDefault();
   if (validFieldsForm() === -1) {
-    if (rol.value == 1) {
-      console.log("1");
-      window.location.href = "../../Certificado/Certificado-main/perfiladministrador.html";
+    if (AdmiFieldsForm() === -1) {
+      window.location.href = "../Certificado-main/perfiladministrador.html";
     }
     else if (rol.value == 2) {
-      console.log("2");
-      window.location.href = "../../Certificado/Certificado-main/Docente.html";
+      window.location.href = "../Certificado-main/Docente.html";
     }
     else if (rol.value == 3) {
-      console.log("3");
-      window.location.href = "../../Certificado/Certificado-main/Catedratico.html";
+      window.location.href = "../Certificado-main/Catedratico.html";
     }
     else if (rol.value == 4) {
-      console.log("4");
-      window.location.href = "../../Certificado/Certificado-main/Practicante.html";
+      window.location.href = "../Certificado-main/Practicante.html";
     }
   } else {
-    modalAlerta("Error");
+    modalAlerta("Error en los datos");
   }
 
 })
-
 const validFieldsForm = () => {
   const values = Object.values(validFields);
   let response = values.findIndex(e => e === false);
   return response;
 }
-
+const AdmiFieldsForm = () => {
+  const values = Object.values(admiFields);
+  let response = values.findIndex(e => e === false);
+  return response;
+}
+//Objeto de validación para administrador
+let admiFields={
+  admEmail: false,
+  admPass:false,
+  admRol:false
+}
 //Objeto de Validación
-
 let validFields = {
   email: false,
   pass: false,
@@ -46,13 +52,21 @@ let validFields = {
 
 email.addEventListener('change', function(e) {
   const inputEmail = e.target.value;
-  const patronEmail = /^[a-zA-Z.]+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm
+  const patronEmail = /^[a-zA-Z.]+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm;
   validFields.email = inputEmail.match(patronEmail) ? true : false;
   console.log(Object.values(validFields));
   if (validFields.email === false) {
     modalAlerta("El correo no es institucional");
   }
-
+});
+//EMAIL PARA ADMINISTRADOR
+email.addEventListener('change', function(e) {
+  const inputEmail = e.target.value;
+  const patronEmail = /^(p)(v)(a)(l)(e)(n)(c)(i)(a)+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm;
+  const patronEmail2 = /^(l)(l)(i)(z)(a)(r)(a)(z)(o)+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm;
+  admiFields.admEmail = inputEmail.match(patronEmail) ? true : false;
+  admiFields.admEmail = inputEmail.match(patronEmail2) ? true : false;
+  console.log(Object.values(admiFields));
 });
 pass.addEventListener('change', (e) => {
   const inputPass = e.target.value;
@@ -60,12 +74,26 @@ pass.addEventListener('change', (e) => {
   validFields.pass = inputPass.match(patronPass) ? true : false;
   console.log(Object.values(validFields));
 });
+//CONTRASEÑA PARA ADMINISTRADOR
+pass.addEventListener('change', (e) => {
+  const inputPass = e.target.value;
+  const patronPass = /([a-zA-Z0-9]{8,})$/g;
+  admiFields.admPass = inputPass.match(patronPass) ? true : false;
+  console.log(Object.values(admiFields));
+});
 rol.addEventListener('change', function(e) {
   validFields.rol = rol.selectedIndex > 0 ? true : false;
-  if (validFields.rol == false) {
+  if (validFields.rol == false && rol.value==0) {
     modalAlerta("Seleccione un rol")
   }
   console.log(Object.values(validFields));
+});
+rol.addEventListener('change', function(e) {
+  admiFields.admRol = rol.selectedIndex == 1 ? true : false;
+  if (admiFields.admRol == false) {
+    modalAlerta("Seleccione un rol")
+  }
+  console.log(Object.values(admiFields));
 });
 
 function modalAlerta(cadena) {
