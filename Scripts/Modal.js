b005121642835fed08a13375d17f27bc30740bc3
Santsjj <109623621+Santsjj@users.noreply.github.com>
2023-01-10 07:57:36 -0600

    Add files via upload

--- Scripts/Modal.js
+++ Scripts/Modal.js
@@ -1,8 +1,9 @@
+//Creación de alerta modal por medio de la etiqueta <dialog></dialog>
 'use strict';
 const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
 const btnAbrirModal = document.querySelector("#btn-abrir-modal");
 const modal = document.querySelector("#modal");
-
+//Acción para abrir y cerrar alertas (<dialog></dialog>)
 btnAbrirModal.addEventListener("click",(e)=>{
     e.preventDefault();
     modal.showModal();})
--- Scripts/ValidateExpDoc.js
+++ Scripts/ValidateExpDoc.js
@@ -1,4 +1,6 @@
-//Validación
+//Se crea una variable para traer la etiqueta <dialog></dialog>
+const form=document.getElementById("modal")
+//Acción cuando se finalice la validación
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (validFieldsForm() === -1) {
@@ -16,7 +18,10 @@ const validFieldsForm = () => {
 let validFields = {
   FechaExp: false
 }
+//Se crea una variable para traer el input 
 const FechaExp = document.getElementById("FechaExp");
+//validación por medio de la fecha de expriación de un documento
+//No permitirá generar el pdf si no coloca una fecha
 FechaExp.addEventListener('change', (e) => {
   validFields.FechaExp = FechaExp.constructor ? true : false;
   if(validFields==false){
@@ -24,7 +29,7 @@ FechaExp.addEventListener('change', (e) => {
   }
   console.log(Object.values(validFields));
 })
-
+//Creación de alerta
 function modalAlerta(cadena) {
   const alert = document.createElement("div");
   const texto = document.createElement("p");
