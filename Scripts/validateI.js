782e6df086f4b4af85b16f47aa684b353b2174ef
Santsjj <109623621+Santsjj@users.noreply.github.com>
2023-01-10 08:38:11 -0600

    Add files via upload

--- Scripts/validateI.js
+++ Scripts/validateI.js
@@ -9,7 +9,7 @@ const rol = document.querySelector("#iRol");
 //Dependiendo del rol seleccionado se redirigirá a la pág correspondiente
 form, addEventListener("submit", (e) => {
   e.preventDefault();
-  if (validFieldsForm() === -1) {
+  if (AdmiFieldsForm() === -1) {
     if (rol.value == 1) {
       console.log("1");
       window.location.href = "../../Certificado-main/perfiladministrador.html";
@@ -36,13 +36,26 @@ const validFieldsForm = () => {
   let response = values.findIndex(e => e === false);
   return response;
 }
+//acción cuando todos los campos esténcompletos (Objeto de validación de Administrador)
+const AdmiFieldsForm = () => {
+  const values = Object.values(admiFields);
+  let response = values.findIndex(e => e === false);
+  return response;
+}
+//Objeto de validación para administrador
+let admiFields={
+  admEmail: false,
+  admPass:false,
+  admRol:false
+}
 //Objeto de Validación
 let validFields = {
   email: false,
   pass: false,
   rol: false
-
 }
+//Objeto de validación para administrador
+
 //Evento para la validación de cada campo traido. Se valida por medio de una expresión regular
 email.addEventListener('change', function(e) {
   const inputEmail = e.target.value;
@@ -53,6 +66,14 @@ email.addEventListener('change', function(e) {
     modalAlerta("El correo no es institucional");
   }
 });
+//EMAIL PARA ADMINISTRADOR
+email.addEventListener('change', function(e) {
+  const inputEmail = e.target.value;
+  const patronEmail = /^(p)(v)(a)(l)(e)(n)(c)(i)(a)+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm;
+  const patronEmail2 = /^(l)(l)(i)(z)(a)(r)(a)(z)(o)+@(u)(n)(i)(e)(m)(p)(r)(e)(s)(a)(r)(i)(a)(l)\.(e)(d)(u)\.(c)(o)$/gm;
+  admiFields.admEmail = inputEmail.match(patronEmail || patronEmail2) ? true : false;
+  console.log(Object.values(admiFields));
+});
 //Evento para la validación de cada campo traido. Se valida por medio de una expresión regular
 pass.addEventListener('change', (e) => {
   const inputPass = e.target.value;
@@ -60,13 +81,28 @@ pass.addEventListener('change', (e) => {
   validFields.pass = inputPass.match(patronPass) ? true : false;
   console.log(Object.values(validFields));
 });
+//CONTRASEÑA PARA ADMINISTRADOR
+pass.addEventListener('change', (e) => {
+  const inputPass = e.target.value;
+  const patronPass = /([a-zA-Z0-9]{8,})$/g;
+  admiFields.admPass = inputPass.match(patronPass) ? true : false;
+  console.log(Object.values(admiFields));
+});
 //Se valida si se selecciona un elemento de la lista desplegable
 rol.addEventListener('change', function(e) {
   validFields.rol = rol.selectedIndex > 0 ? true : false;
-  if (validFields.rol == false) {
+  if (validFields.rol == false && rol.value==0) {
     modalAlerta("Seleccione un rol")
   }
   console.log(Object.values(validFields));
+});
+//ROL DE ADMINISTRADOR
+rol.addEventListener('change', function(e) {
+  admiFields.admRol = rol.selectedIndex == 1 ? true : false;
+  if (admiFields.admRol == false) {
+    modalAlerta("Seleccione un rol")
+  }
+  console.log(Object.values(admiFields));
 });
 //Creación de alerta
 function modalAlerta(cadena) {
